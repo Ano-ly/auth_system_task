2ece5763bc17e0b1a2b12c478f5a0e2a82b03f85
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2025-07-08 07:09:20 +0100

    Add configurations

---
+++ config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    """Holds database, JWT and Mail configuration"""
+
+    SECRET_KEY = os.getenv('SECRET_KEY')
+
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES_SECONDS', 3600))
+    JWT_REFRESH_TOKEN_EXPIRES = int(os.getenv('JWT_REFRESH_TOKEN_EXPIRES_DAYS', 30))
+
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = int(os.getenv('MAIL_PORT', 2525))
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'True').lower() == 'true'
+    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL', 'False').lower() == 'true'
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
